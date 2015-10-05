c3239d8b01585ac627cf67293e6f3a699b2cd6dc
Jesse Soldat <jsoldat@hotmail.com>
2015-10-05 16:16:38 -0400

    both clocks and the css are finished

--- main.js
+++ main.js
@@ -1,10 +1,63 @@
-var heading2 = $('h2');
+function setTime(){
+var timeNow = new Date();
+// console.log(timeNow);
 
-$('h2').addClass('cool');
+var hoursNow = timeNow.getHours();
+var minutesNow = timeNow.getMinutes();
+var secondsNow = timeNow.getSeconds();
+//leading 0 for minutes and seconds if needed
+  if(minutesNow < 10) {
+    minutesNow = '0'+minutesNow;
+  } 
 
-$('li').addClass('listItem');
+ if(secondsNow < 10) {
+    // console.log(secondsNow);
+    secondsNow = '0'+secondsNow;
+  }
 
-var lis = document.querySelectorAll('li');
-lis.forEach(function(li){
-  li.classList.add('item');
-})
+var currentTime = hoursNow+":"+minutesNow+":"+secondsNow;
+
+ // console.log(currentTime);
+$('.clock').html(currentTime);
+}
+window.onload = setTime();
+setInterval('setTime()', 1000);
+
+//clock to hex + color
+
+function hexColor(){ 
+  var hexTime = new Date();
+  
+  var hexHours = hexTime.getHours();
+  var hexMinutes = hexTime.getMinutes();
+  var hexSeconds = hexTime.getSeconds();
+
+  if (hexMinutes < 10) {
+    hexMinutes = '0'+hexMinutes;
+  }
+  if (hexSeconds < 10) {
+    hexSeconds = '0'+hexSeconds;
+  }
+
+  var hexFinalTime = '#'+hexHours+hexMinutes+hexSeconds;
+  // console.log(hexFinalTime);
+  $('.hexClock').html(hexFinalTime);
+
+  $('.backgroundColor').css('background-color', hexFinalTime);
+  $('h1').css('color', 'white');
+
+  
+}
+window.onload = hexColor();
+setInterval('hexColor()', 1000);
+
+
+
+
+
+
+
+
+
+
+
